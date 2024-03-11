016e5e6a09ff3c3c4e45db0a07c7c0856f62a251
Darknab <117382045+Darknab@users.noreply.github.com>
2024-03-11 18:43:56 +0100

    Handle created buttons and forecast card

--- script.js
+++ script.js
@@ -24,17 +24,13 @@ let prefTemp = tempBtn.textContent;
 let prefWind = windBtn.textContent;
 
 tempBtn.addEventListener('click', () => {
-  if (tempBtn.textContent === 'Celcius') {
+  if (tempBtn.textContent === 'Celsius') {
     tempBtn.textContent = 'Farenheit';
   } else {
-    tempBtn.textContent = 'Celcius';
+    tempBtn.textContent = 'Celsius';
   }
   prefTemp = tempBtn.textContent;
-
-  // Update the displayed ddata if any data is displayed
-  if ( document.querySelector('#location').textContent !== '') {
-    searchLocation(search.value);
-  }
+  displayData(weather);
 });
 
 windBtn.addEventListener('click', () => {
@@ -44,9 +40,7 @@ windBtn.addEventListener('click', () => {
     windBtn.textContent = 'Km/h';
   }
   prefWind = windBtn.textContent;
-  if ( document.querySelector('#location').textContent !== '') {
-    searchLocation(search.value);
-  }
+  displayData(weather);
 });
 
 // Fetch and display data from API
@@ -61,32 +55,83 @@ async function getWeather(city = 'Tlemcen') {
   }
 }
 
+let weather;
+
+getWeather().then((response) => {
+  weather = response;
+  displayData(weather);
+});
+
 function searchLocation(location = 'Tlemcen') {
   getWeather(location).then((response) => {
-    setId('location', `${response.location.name}, ${response.location.country}`);
-    const date = new Date(response.location.localtime);
-    setId('time', `${dayWeek(date.getDay())}, ${month(date.getMonth())} ${dayMonth(date.getUTCDate())} ${date.getFullYear()}`);
-    setId('now', `${date.getHours()}:${date.getMinutes()}`);
-    setId('condition', response.current.condition.text);
-    if (prefTemp === 'Celcius') {
-      setId('temperature', `${response.current.temp_c}°C`);
-    } else {
-      setId('temperature', `${response.current.temp_f}°F`);
-    }
-    setId('humidity', `Humidity: ${response.current.humidity}%`);
-    if (prefWind === 'Km/h') {
-      setId('wind', `Wind: ${response.current.wind_dir}, ${response.current.wind_kph}Km/h`)
-    } else {
-      setId('wind', `Wind: ${response.current.wind_dir}, ${response.current.wind_mph}Mph`)
-    }
-    document.querySelector('.card-now').style.backgroundImage = `url(${response.current.condition.icon})`;
+    weather = response;
+    displayData(weather);
  })
 }
 
-// Only for  visualizing data 
-getWeather('Tlemcen').then((response) => {
-  console.log(response);
+function displayData(response) {
+  setId('location', `${response.location.name}, ${response.location.country}`);
+  displayNowCard(response);
+  const date2 = new Date(response.forecast.forecastday[2].date);
+  setId('day0', `Today`);
+  setId('day1', `Tomorrow`);
+  setId('day2', `${dayWeek(date2.getDay())}`);
+  displayForecast(response.forecast.forecastday[day]);
+
+}
+
+function displayNowCard(response) {
+  const date = new Date(response.forecast.forecastday[day].date);
+  setId('time', `${dayWeek(date.getDay())}, ${month(date.getMonth())} ${dayMonth(date.getUTCDate())} ${date.getFullYear()}`);
+  const now = new Date(response.current.last_updated)
+  setId('now', `${now.getHours()}:${now.getMinutes()}`);
+  setId('condition', response.current.condition.text);
+  if (prefTemp === 'Celsius') {
+    setId('temperature', `${response.current.temp_c}°C`);
+  } else {
+    setId('temperature', `${response.current.temp_f}°F`);
+  }
+  setId('humidity', `Humidity: ${response.current.humidity}%`);
+  if (prefWind === 'Km/h') {
+    setId('wind', `Wind: ${response.current.wind_dir}, ${response.current.wind_kph}Km/h`)
+  } else {
+    setId('wind', `Wind: ${response.current.wind_dir}, ${response.current.wind_mph}Mph`)
+  }
+  document.querySelector('.card-now').style.backgroundImage = `url(${response.current.condition.icon})`;
+}
+
+let day = 0;
+const selectBtns = document.querySelectorAll('.select-day');
+selectBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    day = parseInt(btn.id.slice(-1));
+    displayData(weather);
+    document.querySelector('.active').classList.remove('active');
+    btn.classList.add('active');
+  })
 })
+
+function displayForecast(response) {
+  document.querySelector('.card-forecast').style.backgroundImage = `url(${response.day.condition.icon})`
+  setId('forecast-condition', response.day.condition.text);
+  if (prefTemp === 'Celsius') {
+    setId('min-temp', `min: ${response.day.mintemp_c}°C`);
+    setId('max-temp', `max: ${response.day.maxtemp_c}°C`);
+  } else {
+    setId('min-temp', `min: ${response.day.mintemp_f}°F`);
+    setId('max-temp', `max: ${response.day.maxtemp_f}°F`);
+  }
+  if (response.day.daily_will_it_rain === 1) {
+    setId('rain-chance', `Chances for rain: ${response.day.daily_chance_of_rain}%`);
+  } else {
+    setId('rain-chance', ``);
+  }
+  if (response.day.daily_will_it_snow === 1) {
+    setId('snow-chance', `Chances for rain: ${response.day.daily_chance_of_snow}%`);
+  } else {
+    setId('snow-chance', ``);
+  }
+}
 
 // date functions
 function dayWeek(value) {
@@ -174,4 +219,3 @@ function month(value) {
 
   return result;
 }
-
